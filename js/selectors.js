e3bed1b869c56297748d38163e27bdab0f7ea9c3
bnreinecke0209@gmail.com <bnreinecke0209@gmail.com>
2018-07-23 23:30:15 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -7,12 +7,5 @@ function lastImageSelector() {
 }
 
 /*
-  it('lastImageSelector() should return the last image', function() {
-    expect(lastImageSelector().length).toBe(1);
-    expect(lastImageSelector().attr('src')).toEqual("https://s3.amazonaws.com/after-school-assets/confused.gif");
-  });
 
-+ Write a function `lastImageSelector` that does not accept any parameters. The
-function should use the last jQuery selector to return the last image in
-`index.html`.
 */
