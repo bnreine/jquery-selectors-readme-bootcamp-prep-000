5a5e08d1a723d7408441947aae6f13120378a7ea
bnreinecke0209@gmail.com <bnreinecke0209@gmail.com>
2018-07-23 23:34:21 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -14,12 +14,7 @@ function divSelector() {
   return $('div.pics');
 }
 
-/*
-  it('divSelector() should return two divs', function() {
-    expect(divSelector().length).toBe(2);
-    expect(divSelector()[0].tagName).toEqual("DIV");
-  });
 
-+ Write a function `divSelector` that does not accept any parameters. The function
-should use a class selector to return the two divs with the class `pics`.
+/*
+
 */
