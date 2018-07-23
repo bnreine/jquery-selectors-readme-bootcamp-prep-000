cc8475974a6adf6477cdb03049c5acebfaba3dcf
bnreinecke0209@gmail.com <bnreinecke0209@gmail.com>
2018-07-23 23:28:25 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -2,11 +2,15 @@ function paragraphSelector() {
   return $('p');
 }
 
-
+function lastImageSelector() {
+  return $('')
+}
 
 /*
-
-
+  it('lastImageSelector() should return the last image', function() {
+    expect(lastImageSelector().length).toBe(1);
+    expect(lastImageSelector().attr('src')).toEqual("https://s3.amazonaws.com/after-school-assets/confused.gif");
+  });
 
 + Write a function `lastImageSelector` that does not accept any parameters. The
 function should use the last jQuery selector to return the last image in
