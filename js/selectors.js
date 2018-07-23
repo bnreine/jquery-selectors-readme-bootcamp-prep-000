32e5e6fa1c8e2c90c3f7f31e7e8fc3488e1116f4
bnreinecke0209@gmail.com <bnreinecke0209@gmail.com>
2018-07-23 23:33:24 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -10,10 +10,15 @@ function ninjaBabySelector() {
   return $('#baby-ninja');
 }
 
-
+function divSelector() {
+  
+}
 
 /*
-
+  it('divSelector() should return two divs', function() {
+    expect(divSelector().length).toBe(2);
+    expect(divSelector()[0].tagName).toEqual("DIV");
+  });
 
 + Write a function `divSelector` that does not accept any parameters. The function
 should use a class selector to return the two divs with the class `pics`.
