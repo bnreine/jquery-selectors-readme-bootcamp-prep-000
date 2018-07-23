c5c0e462e43bd770d5e9d02e9af93a40701a6038
bnreinecke0209@gmail.com <bnreinecke0209@gmail.com>
2018-07-23 23:32:33 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -10,12 +10,8 @@ function ninjaBabySelector() {
   return $('#baby-ninja');
 }
 
-/*
-  it('ninjaBabySelector() should return the image of the ninja baby', function() {
-    expect(ninjaBabySelector().length).toBe(1);
-    expect(ninjaBabySelector().attr('src')).toEqual("https://s3.amazonaws.com/after-school-assets/baby-ninja.gif");
-  });
 
-+ Write a function `ninjaBabySelector` that does not accept any parameters. The
-function should use an ID selector to return the ninja baby image.
+
+/*
+
 */
