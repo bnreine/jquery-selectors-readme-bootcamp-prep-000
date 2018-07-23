8ac6fecbfd36774e8cf36bcc3a4f52fad683bd94
bnreinecke0209@gmail.com <bnreinecke0209@gmail.com>
2018-07-23 23:35:01 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -14,9 +14,17 @@ function divSelector() {
   return $('div.pics');
 }
 
+function firstListItem() {
+  
+}
 
 /*
+  it('firstListItem() should return the first list item in the unordered list', function() {
+    expect(firstListItem().length).toBe(1);
+    expect(firstListItem()[0].children[0].src).toEqual('https://s3.amazonaws.com/after-school-assets/awesome.jpg');
+  });
 
+});
 
 + Write a function `firstListItem` that does not accept any parameters. The
 function should use a first-child selector to return the first list item in the
