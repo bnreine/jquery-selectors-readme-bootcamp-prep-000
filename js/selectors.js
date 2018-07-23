111eb4a4608fb109006dafdc68940833c1f05b13
bnreinecke0209@gmail.com <bnreinecke0209@gmail.com>
2018-07-23 23:34:37 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -17,4 +17,9 @@ function divSelector() {
 
 /*
 
+
++ Write a function `firstListItem` that does not accept any parameters. The
+function should use a first-child selector to return the first list item in the
+`ul` with the ID `pic-list`.
+
 */
