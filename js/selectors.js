ff96c05a1df3d3e24fc6ebd256d654e09f78821a
bnreinecke0209@gmail.com <bnreinecke0209@gmail.com>
2018-07-23 23:23:06 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -2,7 +2,14 @@
 
 
 
-/*+ Write a function `paragraphSelector` that does not accept any parameters. The
+/*
+describe('Selectors', () => {
+  it('paragraphSelector() should select all paragraphs', function() {
+    expect(paragraphSelector().length).toBe(1);
+    expect(paragraphSelector()[0].innerHTML).toEqual($('p')[0].innerHTML);
+  });
+
++ Write a function `paragraphSelector` that does not accept any parameters. The
 function should use an element selector to select the `p` tags in `index.html`
 and return those elements. (Note that we're using the plural because the
 selector will always return an array when used with tags, even if there's only
